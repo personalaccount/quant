a04a2572125c5376bfc1c1282f2a4d0f5b3c1835
Philip <philip.i@outlook.com>
2014-10-23 01:41:22 +0300

    Add file

---
+++ market-simulator.py
@@ -0,0 +1,10 @@
+'''
+It's a python program that accepts market orders in a .CSV format.
+Once it reads in the data it processes the orders and assesses the portfolio that is created by processing these orders.
+Therefore calculating the Total Return, Sharpe Ratio, Standard Deviation, 
+'''
+
+
+
+# 1. Read in the file of trades in an numpy array
+# 2. Build the list of all symbols that are traded 
